204adb0f2e06e37b7a7fa499d1e4471eb5c576cd
Subash Pradahan <suprowilde5@gmail.com>
2019-07-22 19:13:42 +0200

    User story one completed with react-redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Title from './components/Title';
+import DogListContainer from './components/DogListContainer';
+
+
 
 function App() {
   return (
     <div className="App">
       <Title name='All Dog Breeds'/>
+      <DogListContainer />
     </div>
   );
 }
---
+++ src/components/DogListContainer.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import DogList from './DogList'
+import { connect } from 'react-redux';
+// import { Link } from 'react-router-doom'
+import { displayDogs } from '../actions/displayDogList'
+
+class DogListContainer extends Component {
+  // state = { dogBreeds: null }
+
+  componentDidMount() {
+    this.props.displayDogs()
+  }
+
+  render() {
+    console.log(this.props)
+    if (!this.props.dogBreeds) return 'Loading...'
+    else {
+      return <DogList dogBreeds={this.props.dogBreeds} />
+    }
+  }
+}
+
+const mapstateToProps = (state) => {
+  return {
+    dogBreeds: state
+  }
+}
+export default connect(mapstateToProps, { displayDogs })(DogListContainer)
+
