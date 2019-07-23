8540e3f2dfedf6259d1eec7e60a9b1d23f18f2dd
gergokutu <gergo.kutu@gmail.com>
2019-07-23 10:22:12 +0200

    link removed

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
-import { Link } from 'react-router-dom'
 
 
 ReactDOM.render(
