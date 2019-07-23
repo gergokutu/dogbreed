e8557d783de68de426495b8a6a76b5667083bf26
Subash Pradahan <suprowilde5@gmail.com>
2019-07-23 10:13:05 +0200

    making clickable links and adding up files for image-details

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-import {Provider} from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import store from './store'
+import { Link } from 'react-router-dom'
+
 
 ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
+	<BrowserRouter>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</BrowserRouter>,
 	document.getElementById('root')
 )
