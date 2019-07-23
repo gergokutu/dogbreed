7fb5a32273b287422d9337e3100105a22ebeb8a7
Suhas <suhaskn09@gmail.com>
2019-07-23 13:43:18 +0200

    Added button component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Title from './components/Title';
 import DogListContainer from './components/DogListContainer';
 import { Route } from 'react-router-dom'
 import DogBreedImages from './components/DogBreedImages'
+import Button from './components/Button'
 
 function App() {
   return (
     <div className="App">
       <Title name='All Dog Breeds'/>
+      <Button />
       <Route exact path="/" component={DogListContainer} />
       <Route path="/dog-breeds/:breed" component={DogBreedImages} />
 
