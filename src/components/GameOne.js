db44cc2ace9d52c3913041bd7f54fb05b81151a4
gergokutu <gergo.kutu@gmail.com>
2019-07-24 13:43:14 +0200

    good breed name under the picure

--- src/components/GameOne.js
+++ src/components/GameOne.js
@@ -3,7 +3,13 @@ import { Link } from 'react-router-dom'
 
 export default function GameOne(props) {
   const { picture } = props
-  console.log("Pictures", picture)
+  // picture is an array » we need a string!
+  const string = picture.toString()
+  // we have to split that string (the url) by '/'...
+  // ...and we have to get the breed name which is the 5th...
+  // ...so we need [4] in <p>
+  const breedName = string.split('/')
+  console.log("breedName:", breedName)
   return (
     <div className="game-one">
       <h1>GAME ONE</h1>
@@ -11,7 +17,9 @@ export default function GameOne(props) {
       <div>
         {<img src={picture} alt="Dog" />}
         {!picture && 'Loading...'}
-
+        {<ul>
+          <li key='goodAnswer'>{breedName[4]}</li>
+        </ul>}
       </div>
       <Link to="/">Go back to the index</Link>
     </div>
