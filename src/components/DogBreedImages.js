88b068e49d844598a6f1757fc139173323246392
gergokutu <gergo.kutu@gmail.com>
2019-07-25 22:48:50 +0200

    more userfriendly

--- src/components/DogBreedImages.js
+++ src/components/DogBreedImages.js
@@ -7,11 +7,11 @@ export default function DogBreedImages(props) {
   return (
     <div className="dog-breed-images">
       <p>This page will shows images of the {name} breed.</p>
+      <Link to="/">Go back to the index</Link>
       <div>
         {images && images.map((url, id) => <img src={url} alt="Dog" key={id} />)}
         {!images && 'Loading...'}
       </div>
-      <Link to="/">Go back to the index</Link>
     </div>
   )
-  }
+}
