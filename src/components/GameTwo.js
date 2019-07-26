df72ab9e2b6bdab094de536ee8851a3db7d0ec99
Subash Pradahan <suprowilde5@gmail.com>
2019-07-26 14:43:26 +0200

    game two with new questions ready

--- src/components/GameTwo.js
+++ src/components/GameTwo.js
@@ -1,66 +1,70 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import {connect} from 'react-redux'
-import {displayRandomNames} from '../actions/gameTwo'
-import {displayImagesToAnswer} from '../actions/twoRandomImages'
-import {displayRightImage} from '../actions/getRightImage'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { displayRandomNames } from '../actions/gameTwo'
+import { displayImagesToAnswer } from '../actions/twoRandomImages'
+import { displayRightImage } from '../actions/getRightImage'
 
-function GameTwo(props){
-  const {gameTwo} = props
-
-  console.log('twoPictures test:', gameTwo.twoPictures)
-  console.log('props.breedName test:', props.breedName)
+function GameTwo(props) {
+  const { gameTwo } = props
 
   const shuffledPictures = gameTwo
     .twoPictures
     .concat(props.breedName)
-    .sort(()=> Math.random() - 0.5)
+    .sort(() => Math.random() - 0.5)
 
   console.log('shuffledPictures test:', shuffledPictures)
 
   const correctPic = (data) => {
     if (data.url) {
-      alert('great')
+      alert('Correct! Click OK to continue the test.')
+      props.displayRandomNames()
+      props.displayImagesToAnswer()
     } else {
-      alert('sorry')
+      console.log('data', data)
+      alert(`Wrong, the correct answer is: ${gameTwo.name}`)
+      setTimeout(() => {
+        props.displayRandomNames()
+        props.displayImagesToAnswer()
+      }, 1000)
     }
   }
 
-    return(
-      <div className="gameTwo">
-        <h1>GAME TWO</h1>
-        <Link to="/">Go back to the Homepage</Link>
-        <p>Select the right Image below</p>
-    
-        <div className="randomNames">
-          {!gameTwo.name && 'Loading...'}
-          <h2><b>{gameTwo.name}</b></h2>
-        </div>
-        
-        <div className="ImageAnswers">
-          {
-            shuffledPictures
-              .map((data) => {
-                const url = data.url ? data.url : data
-                
-                return <img
-                  src={url}
-                  alt='Dog'
-                  onClick={() => correctPic(data)}
-                  key={url}
-                />
-              })
-          }
-        </div>
+  return (
+    <div className="gameTwo">
+      <h1>GAME TWO</h1>
+      <Link to="/">Go back to the Homepage</Link>
+      <p>Select the right Image below</p>
 
-      
+      <div className="randomNames">
+        {!gameTwo.name && 'Loading...'}
+        <h2><b>{gameTwo.name}</b></h2>
       </div>
-    )
-  }
+
+      <div className="ImageAnswers">
+        {
+          shuffledPictures
+            .map((data) => {
+              const url = data.url ? data.url : data
+
+              return <img
+                src={url}
+                alt='Dog'
+                onClick={() => correctPic(data)}
+                key={url}
+              />
+            })
+        }
+      </div>
+
+
+    </div>
+  )
+}
 const mapStateToProps = (state) => {
-  return{
+  return {
     ...state
   }
 }
 
-export default connect(mapStateToProps,{displayRandomNames, displayImagesToAnswer, displayRightImage})(GameTwo)
+export default connect(mapStateToProps, { displayRandomNames, displayImagesToAnswer, displayRightImage })(GameTwo)
